cdfe91a8609844ad2c0903c830febed8e20738bc
Jiefeng <lowjiefeng1998@gmail.com>
2020-09-19 15:49:43 +0800

    feat: add person entity

---
+++ src/entities/user/Person.ts
@@ -0,0 +1,67 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+} from "class-validator";
+import { Column, Entity, OneToOne } from "typeorm";
+import { Discardable } from "../Discardable";
+import { User } from "../User";
+import { Gender } from "./Gender";
+
+@Entity()
+export class Person extends Discardable {
+  entityName = "Person";
+
+  constructor(
+    name: string,
+    gender: Gender,
+    email?: string,
+    mobile_number?: string,
+    home_number?: string,
+    birthday?: null,
+    user?: User
+  ) {
+    super();
+    this.name = name;
+    this.gender = gender;
+    this.email = email ? email : null;
+    this.mobile_number = mobile_number ? mobile_number : null;
+    this.home_number = home_number ? home_number : null;
+    this.birthday = birthday ? birthday : null;
+    this.user = user ? user : null;
+  }
+
+  @Column()
+  @IsNotEmpty()
+  name: string;
+
+  @Column({
+    type: "enum",
+    enum: Gender,
+  })
+  gender: Gender;
+
+  @Column({ type: "character varying", nullable: true })
+  @IsOptional()
+  @IsEmail()
+  email: string | null;
+
+  @Column({ type: "character varying", nullable: true })
+  @IsOptional()
+  @IsPhoneNumber("SG")
+  mobile_number: string | null;
+
+  @Column({ type: "character varying", nullable: true })
+  @IsOptional()
+  @IsPhoneNumber("SG")
+  home_number: string | null;
+
+  @Column({ type: "date", nullable: true })
+  birthday: Date | null;
+
+  @OneToOne((type) => User, { nullable: true })
+  user: User | null;
+
+  // TODO: getData()
+}
---
+++ src/entitiesTest/user/person.test.ts
@@ -0,0 +1,82 @@
+import { validate } from "class-validator";
+import { postgres } from "../../../ormconfig";
+import { Connection, createConnection } from "typeorm";
+import { Gender } from "../../entities/user/Gender";
+import { Person } from "../../entities/user/Person";
+import { User } from "../../entities/User";
+
+let connection: Connection;
+
+beforeAll(async () => {
+  connection = await createConnection(postgres);
+});
+
+afterEach(async () => {
+  const personRepository = connection.getRepository(Person);
+  const userRepository = connection.getRepository(User);
+
+  await personRepository.delete({});
+  await userRepository.delete({});
+});
+
+afterAll(async () => {
+  await connection.close();
+});
+
+describe("Create person", () => {
+  it("with valid name", async () => {
+    let person: Person;
+
+    person = new Person("Bobby", Gender.MALE);
+    const errors = await validate(person);
+    expect(errors.length).toBe(0);
+
+    const newPerson = await connection.getRepository(Person).save(person);
+    expect(newPerson).toBeTruthy();
+  });
+
+  it("with valid email", async () => {
+    let person: Person;
+
+    person = new Person("Bobby", Gender.MALE, "bobby@example.com");
+    const errors = await validate(person);
+    expect(errors.length).toBe(0);
+  });
+
+  it("with invalid email", async () => {
+    let person: Person;
+
+    person = new Person("Bobby", Gender.MALE, "not-an-email");
+    const errors = await validate(person);
+    expect(errors.length).toBe(1);
+  });
+
+  it("with valid mobile_phone", async () => {
+    let person: Person;
+
+    person = new Person("Bobby", Gender.MALE, undefined, "91234567");
+    const errors = await validate(person);
+    expect(errors.length).toBe(0);
+  });
+
+  it("with invalid mobile_phone", async () => {
+    let person: Person;
+
+    person = new Person("Bobby", Gender.MALE, undefined, "9123456");
+    const errors = await validate(person);
+    expect(errors.length).toBe(1);
+  });
+
+  it("with user", async () => {
+    const user = new User("Bobby", "Bobby");
+    const person = new Person("Bobby Tan", Gender.MALE);
+
+    const newUser = await connection.getRepository(User).save(user);
+
+    person.user = newUser;
+    const newPerson = await connection.getRepository(Person).save(person);
+
+    expect(newPerson).toBeTruthy();
+    expect(newPerson.user?.id).toBe(newUser.id);
+  });
+});
