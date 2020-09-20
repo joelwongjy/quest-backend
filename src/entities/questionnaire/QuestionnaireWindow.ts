3547528de27c632d139a0a9e60e9bc203147d18f
vineethbuddha <buddhavineeth@gmail.com>
2020-09-21 01:31:25 +0800

    fix: add ManyToOne relation in QuestionnaireWindow

--- src/entities/questionnaire/QuestionnaireWindow.ts
+++ src/entities/questionnaire/QuestionnaireWindow.ts
@@ -1,14 +1,16 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 import { Discardable } from "../Discardable";
+import { Questionnaire } from "./Questionnaire";
 
 @Entity()
 export class QuestionnaireWindow extends Discardable {
   entityName = "QuestionnaireWindow";
 
-  constructor(open_at: Date, close_at: Date) {
+  constructor(open_at: Date, close_at: Date, questionnaire: Questionnaire) {
     super();
     this.open_at = open_at;
     this.close_at = close_at;
+    this.questionnaire = questionnaire;
   }
 
   @Column({ type: "timestamp without time zone" })
@@ -16,4 +18,10 @@ export class QuestionnaireWindow extends Discardable {
 
   @Column({ type: "timestamp without time zone" })
   close_at: Date;
+
+  @ManyToOne(
+    (type) => Questionnaire,
+    (questionnaire) => questionnaire.questionnaire_windows
+  )
+  questionnaire: Questionnaire;
 }
