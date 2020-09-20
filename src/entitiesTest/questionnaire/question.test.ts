ac9176d27670e2981f6bd299bab352e150530a5c
vineethbuddha <buddhavineeth@gmail.com>
2020-09-20 14:18:07 +0800

    test: Create tests for Question Entity

---
+++ src/entitiesTest/questionnaire/question.test.ts
@@ -0,0 +1,41 @@
+import { validate } from "class-validator";
+import { postgres } from "../../../ormconfig";
+import { Connection, createConnection } from "typeorm";
+import { QuestionType } from "../../entities/questionnaire/QuestionType";
+import { Question } from "../../entities/questionnaire/Question";
+
+let connection: Connection;
+
+beforeAll(async () => {
+  connection = await createConnection(postgres);
+});
+
+afterEach(async () => {
+  const questionRepository = connection.getRepository(Question);
+  await questionRepository.delete({});
+});
+
+afterAll(async () => {
+  await connection.close();
+});
+
+describe("Create question", () => {
+  it("with valid question_text", async () => {
+    let question: Question;
+
+    question = new Question("How are you feeling today?", QuestionType.MOOD);
+    const errors = await validate(question);
+    expect(errors.length).toBe(0);
+
+    const newQuestion = await connection.getRepository(Question).save(question);
+    expect(newQuestion).toBeTruthy();
+  });
+
+  it("with an empty question_text", async () => {
+    let question: Question;
+
+    question = new Question("", QuestionType.SHORT_ANSWER);
+    const errors = await validate(question);
+    expect(errors.length).not.toBe(0);
+  });
+});
