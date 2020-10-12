66d9e9407cfe041184e8768b506c36094fac70b7
Jiefeng <lowjiefeng1998@gmail.com>
2020-10-12 21:52:46 +0800

    feat: add users to classes

--- src/controllers/SeedController.ts
+++ src/controllers/SeedController.ts
@@ -2,43 +2,124 @@ import { validateOrReject } from "class-validator";
 import { User } from "../entities/user/User";
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
-import { DefaultUserRole, UserPostData } from "../types/users";
+import { DefaultUserRole } from "../types/users";
 import { Programme } from "../entities/programme/Programme";
 import { Class } from "../entities/programme/Class";
+import { ClassUserRole } from "../types/classUsers";
+import { ClassUser } from "../entities/programme/ClassUser";
+
+const DEFAULT_PASSWORD = "password";
+
+type UserSeed = [string, ClassUserRole];
+
+type ClassUserSeed = {
+  name: string;
+  users: UserSeed[];
+};
+
+type ProgrammeSeed = {
+  name: string;
+  classes: ClassUserSeed[];
+};
+
+const PROGRAMME_SEED: ProgrammeSeed[] = [
+  {
+    name: "Programme A",
+    classes: [
+      {
+        name: "Class A1",
+        users: [
+          ["A1-Teacher", ClassUserRole.TEACHER],
+          ["A1-Student", ClassUserRole.STUDENT],
+        ],
+      },
+      {
+        name: "Class A2",
+        users: [
+          ["A2-Teacher", ClassUserRole.TEACHER],
+          ["A2-Student", ClassUserRole.STUDENT],
+        ],
+      },
+    ],
+  },
+  {
+    name: "Programme B",
+    classes: [
+      {
+        name: "Class B1",
+        users: [
+          ["B1-Teacher", ClassUserRole.TEACHER],
+          ["B1-Student", ClassUserRole.STUDENT],
+        ],
+      },
+      {
+        name: "Class B2",
+        users: [
+          ["B2-Teacher", ClassUserRole.TEACHER],
+          ["B2-Student", ClassUserRole.STUDENT],
+        ],
+      },
+    ],
+  },
+  {
+    name: "Programme C",
+    classes: [
+      {
+        name: "Class C1",
+        users: [
+          ["C1-Teacher", ClassUserRole.TEACHER],
+          ["C1-Student", ClassUserRole.STUDENT],
+        ],
+      },
+      {
+        name: "Class C2",
+        users: [
+          ["C2-Teacher", ClassUserRole.TEACHER],
+          ["C2-Student", ClassUserRole.STUDENT],
+        ],
+      },
+    ],
+  },
+];
 
 export async function seed(
   _request: Request,
   response: Response
 ): Promise<void> {
-  const SUPERUSER = "superuser";
-  const SUPERUSER_NAME = "Superuser";
-  const SUPERUSER_PASSWORD = "password";
-  const superuserResult = await seedUserIfAbsent(
-    SUPERUSER,
-    SUPERUSER_NAME,
-    SUPERUSER_PASSWORD,
+  const superuser = new User(
+    "superuser",
+    "superuser",
+    DEFAULT_PASSWORD,
     DefaultUserRole.ADMIN
   );
-
-  const PROGRAMME_SEED = [
-    { name: "Programme A", classes: ["Class A1", "Class A2"] },
-    { name: "Programme B", classes: ["Class B1", "Class B2"] },
-    { name: "Programme C", classes: ["Class C1", "Class C2"] },
-  ];
-  const programmesClassesResult = await seedProgrammesClasses(PROGRAMME_SEED);
+  await seedUserIfAbsent(superuser);
+  await seedProgrammesClasses(PROGRAMME_SEED);
 
   response.status(200).json({
-    "entities seeded": {
-      superuser: superuserResult,
-      programmes: programmesClassesResult,
+    "Programmes, Classes and Users": {
+      programmes: PROGRAMME_SEED,
     },
   });
 }
 
-type ProgrammeSeed = {
-  name: string;
-  classes: string[];
-};
+async function seedUserIfAbsent(user: User): Promise<User> {
+  const repo = getRepository(User);
+  const { username } = user;
+
+  // return if username already exists
+  const findUser = await repo.findOne({
+    where: { username: username },
+  });
+  if (findUser) {
+    return findUser;
+  }
+
+  // create new user
+  await validateOrReject(user);
+  const seededUser = await repo.save(user);
+
+  return seededUser;
+}
 
 async function seedProgrammesClasses(
   seed: ProgrammeSeed[]
@@ -62,19 +143,19 @@ async function seedProgrammesClasses(
         // save the programme
         const programme = new Programme(name);
         await validateOrReject(programme);
-        const newProgramme = await programmeRepo.save(programme);
-
-        // initiate all classes
-        const classesData = await Promise.all(
-          classes.map(async (className) => {
-            const class_ = new Class(className, newProgramme);
-            await validateOrReject(class_);
-            return class_;
-          })
-        );
+        const seededProgramme = await programmeRepo.save(programme);
 
         // save all classes
-        classRepo.save(classesData);
+        await Promise.all(
+          classes.map(async (classTemplate) => {
+            const { name, users } = classTemplate;
+
+            const class_ = new Class(name, seededProgramme);
+            await validateOrReject(class_);
+            const seededClass = await classRepo.save(class_);
+            await seedClassWithUsers(seededClass, users);
+          })
+        );
       })
     );
 
@@ -84,32 +165,21 @@ async function seedProgrammesClasses(
   }
 }
 
-type UserSeed = Omit<UserPostData, "name">;
+async function seedClassWithUsers(
+  class_: Class,
+  users: UserSeed[]
+): Promise<void> {
+  const classUserRepo = getRepository(ClassUser);
 
-async function seedUserIfAbsent(
-  username: string,
-  name: string,
-  password: string,
-  defaultUserRole: DefaultUserRole = DefaultUserRole.GUEST
-): Promise<UserSeed | string> {
-  const repo = getRepository(User);
+  await Promise.all(
+    users.map(async (userTuple) => {
+      const name = userTuple[0];
+      const user = await seedUserIfAbsent(
+        new User(name, name, DEFAULT_PASSWORD)
+      );
 
-  try {
-    // return if username already exists
-    const userCount = await repo.count({
-      where: { username: username },
-    });
-    if (userCount > 0) {
-      return `${username} already exists`;
-    }
-
-    // create new user
-    const user = new User(username, name, password, defaultUserRole);
-    await validateOrReject(user);
-    await repo.save(user);
-
-    return { username, password, defaultUserRole };
-  } catch (e) {
-    return `Error while seeding User ${username}`;
-  }
+      const classUser = new ClassUser(class_, user, userTuple[1]);
+      await classUserRepo.save(classUser);
+    })
+  );
 }
