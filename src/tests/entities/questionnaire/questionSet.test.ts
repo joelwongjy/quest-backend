dd5712bf0fd9059be58f5eba607c3f179714ee18
Jiefeng <lowjiefeng1998@gmail.com>
2020-10-14 19:17:40 +0800

    test: add query tests for questionSet entity

--- src/tests/entities/questionnaire/questionSet.test.ts
+++ src/tests/entities/questionnaire/questionSet.test.ts
@@ -1,9 +1,11 @@
 import { QuestionSet } from "../../../entities/questionnaire/QuestionSet";
-import { Questionnaire } from "../../../entities/questionnaire/Questionnaire";
-import { QuestionnaireType } from "../../../types/questionnaires";
 import { getRepository } from "typeorm";
 import ApiServer from "../../../server";
 import { synchronize } from "../../../utils/tests";
+import { Question } from "../../../entities/questionnaire/Question";
+import { QuestionType } from "../../../types/questions";
+import { QuestionOrder } from "../../../entities/questionnaire/QuestionOrder";
+import { QuestionnaireWindow } from "../../../entities/questionnaire/QuestionnaireWindow";
 
 let server: ApiServer;
 
@@ -18,47 +20,54 @@ afterAll(async () => {
 });
 
 describe("Create QuestionSet", () => {
-  let questionnaire: Questionnaire;
   let questionSet: QuestionSet;
-
-  beforeAll(async () => {
-    const questionnaireData = new Questionnaire(
-      "Youth Development StudyBuddies",
-      QuestionnaireType.ONE_TIME
-    );
-    questionnaire = await getRepository(Questionnaire).save(questionnaireData);
-  });
 
   afterAll(async () => {
     await synchronize(server);
   });
 
   it("create QuestionSet", async () => {
-    const data = new QuestionSet(questionnaire);
+    const data = new QuestionSet();
     questionSet = await getRepository(QuestionSet).save(data);
 
     expect(questionSet.id).toBeTruthy();
   });
 
-  it("query QuestionSet", async () => {
-    const questionSetQuery = await getRepository(QuestionSet).findOne({
-      where: { id: questionSet.id },
-      relations: ["questionnaire"],
+  it("save questions", async () => {
+    const question = new Question(
+      "How are you today?",
+      QuestionType.SHORT_ANSWER
+    );
+
+    const newQuestion = await getRepository(Question).save(question);
+    expect(newQuestion).toBeTruthy();
+
+    const questionOrder = new QuestionOrder(1, newQuestion);
+    const newQuestionOrder = await getRepository(QuestionOrder).save(
+      questionOrder
+    );
+    expect(newQuestionOrder).toBeTruthy();
+
+    const questionnaireWindow = new QuestionnaireWindow(
+      new Date("2019-05-20"),
+      new Date("2020-05-27")
+    );
+    const newQuestionnaireWindow = await getRepository(
+      QuestionnaireWindow
+    ).save(questionnaireWindow);
+    expect(newQuestionnaireWindow).toBeTruthy();
+
+    const questionSet = new QuestionSet();
+    questionSet.question_orders = [newQuestionOrder];
+    questionSet.questionnaire_windows = [newQuestionnaireWindow];
+    const newQuestionSet = await getRepository(QuestionSet).save(questionSet);
+    expect(newQuestionSet).toBeTruthy();
+
+    const queryQuestionSet = await getRepository(QuestionSet).findOne({
+      where: { id: newQuestionSet.id },
+      relations: ["question_orders", "questionnaire_windows"],
     });
-
-    expect(questionSetQuery?.questionnaire.name).toBe(questionnaire.name);
-  });
-
-  it("query questionnaire", async () => {
-    const questionnaireQuery = await getRepository(Questionnaire).findOne({
-      where: { id: questionnaire.id },
-      relations: ["question_sets", "question_sets.questionnaire"],
-    });
-
-    expect(questionnaireQuery?.question_sets).toHaveLength(1);
-
-    const relatedItem = questionnaireQuery?.question_sets[0];
-
-    expect(relatedItem?.questionnaire.name).toBe(questionnaire.name);
+    expect(queryQuestionSet?.question_orders).toHaveLength(1);
+    expect(queryQuestionSet?.questionnaire_windows).toHaveLength(1);
   });
 });
