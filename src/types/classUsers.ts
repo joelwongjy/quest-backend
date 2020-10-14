6e6a1f0f0ba8c8b6a785c466ef4816012b01c02d
Hanming Zhu <hanming@huawen.com>
2020-10-15 01:52:43 +0800

    feat: add list data and data to classUsers

--- src/types/classUsers.ts
+++ src/types/classUsers.ts
@@ -1,8 +1,18 @@
+import { ClassData } from "./classes";
+import { DiscardableData } from "./entities";
+
 export enum ClassUserRole {
   TEACHER = "Teacher",
   STUDENT = "Student",
   ADMIN = "Admin",
 }
+
+export interface ClassUserListData extends DiscardableData {
+  class: ClassData;
+  role: ClassUserRole;
+}
+
+export interface ClassUserData extends ClassUserListData {}
 
 export function isClassUserRole(role: any): role is ClassUserRole {
   return Object.values(ClassUserRole).includes(role);
