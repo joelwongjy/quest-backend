16274b183d59a9dc6a8fc5ee8b0f73f6124bb98f
Hanming Zhu <hanming@huawen.com>
2020-10-15 02:04:50 +0800

    feat: add some basic data methods and types for Programme

--- src/entities/programme/Programme.ts
+++ src/entities/programme/Programme.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, OneToMany } from "typeorm";
+import { ProgrammeData, ProgrammeListData } from "../../types/programmes";
 import { Discardable } from "../Discardable";
 import { ProgrammeQuestionnaire } from "../questionnaire/ProgrammeQuestionnaire";
 import { Class } from "./Class";
@@ -25,4 +26,13 @@ export class Programme extends Discardable {
     (programmeQuestionnaire) => programmeQuestionnaire.programme
   )
   programmeQuestionnaires!: ProgrammeQuestionnaire[];
+
+  getListData = (): ProgrammeListData => ({
+    ...this.getBase(),
+    name: this.name,
+  });
+
+  getData = (): ProgrammeData => ({
+    ...this.getListData(),
+  });
 }
