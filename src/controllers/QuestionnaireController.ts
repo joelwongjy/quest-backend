4a15e386cfc7aa2afcc54214a9bb6e26844283b3
Jiefeng <lowjiefeng1998@gmail.com>
2020-10-25 20:55:07 +0800

    feat: add authorisation for qnaire/create

--- src/controllers/QuestionnaireController.ts
+++ src/controllers/QuestionnaireController.ts
@@ -6,6 +6,10 @@ import {
   associateQuestionnaireWithClassesAndProgrammes,
   createQuestionnaireWithQuestions,
 } from "../utils/questionnaires";
+import { AccessTokenSignedPayload } from "../types/tokens";
+import { getRepository } from "typeorm";
+import { User } from "../entities/user/User";
+import { DefaultUserRole } from "../types/users";
 
 export async function getQuestionnaireListData(
   _request: Request,
@@ -27,6 +31,17 @@ export async function create(
     classes = [],
     programmes = [],
   } = request.body;
+  const payload = response.locals.payload as AccessTokenSignedPayload;
+  const { userId } = payload;
+
+  const user = await getRepository(User).findOne({
+    where: { id: userId },
+    select: ["id", "defaultRole"],
+  });
+  if (!user || user.defaultRole != DefaultUserRole.ADMIN) {
+    response.sendStatus(401);
+    return;
+  }
 
   let newQuestionnaire = await createQuestionnaireWithQuestions(
     title,
@@ -41,6 +56,5 @@ export async function create(
     newQuestionnaire
   );
 
-  // TODO: this is... weird
   response.status(200).json({ success: true, id: newQuestionnaire.id });
 }
--- src/routes/api/questionnaire.ts
+++ src/routes/api/questionnaire.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import * as QuestionnaireController from "../../controllers/QuestionnaireController";
+import { checkBearerToken } from "../../middlewares/checkBearerToken";
+import { BearerTokenType } from "../../types/tokens";
 
 export const router = Router();
 
 router.get("/questionnaires", QuestionnaireController.getQuestionnaireListData);
+
+router.use(checkBearerToken(BearerTokenType.AccessToken));
 router.post("/create", QuestionnaireController.create);
 
 export default router;
