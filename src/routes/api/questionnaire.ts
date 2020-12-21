893be6ac8266faaa2dfc7d7f2bdd786b90f38366
Hanming Zhu <hanming@huawen.com>
2020-12-21 21:15:15 +0800

    fix: rearrange routes for student access

--- src/routes/api/questionnaire.ts
+++ src/routes/api/questionnaire.ts
@@ -13,14 +13,14 @@ router.get("/", QuestionnaireController.index);
 
 router.use(checkBearerToken(BearerTokenType.AccessToken));
 router.get("/:id/window/:windowId", QuestionnaireController.showWindow);
+router.get("/submissions", AttemptController.index);
+router.get("/submissions/:id", AttemptController.show);
+router.post("/submissions", AttemptController.create);
+router.get("/:id", QuestionnaireController.show);
 
 router.use(checkIfAdmin());
 router.post("/create", QuestionnaireController.create);
 router.delete("/delete/:id", QuestionnaireController.softDelete);
-router.get("/:id", QuestionnaireController.show);
 router.post("/edit/:id", QuestionnaireController.edit);
-
-router.post("/submissions/create", AttemptController.create);
-router.get("/submissions/:id", AttemptController.show);
 
 export default router;
