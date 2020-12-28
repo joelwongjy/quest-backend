8165213a67176868d1239874970cf356691f0b28
Hanming Zhu <hanming@huawen.com>
2020-12-28 15:19:51 +0800

    fix: fix deployment scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       name: "staging",
-      script: "build/index.js",
+      script: "./src/index.js",
       env: {
         NODE_ENV: "development",
       },
     },
     {
       name: "production",
-      script: "build/index.js",
+      script: "./src/index.js",
       env: {
         NODE_ENV: "production",
       },
