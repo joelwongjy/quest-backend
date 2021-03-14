dad231922364364e784e902a6ac95a0cd46e92ea
lowjiefeng1998 <lowjiefeng1998@gmail.com>
2021-03-14 22:31:08 +0800

    feat: add route for class#show

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -1,9 +1,50 @@
 import { Request, Response } from "express";
+import { Class } from "../entities/programme/Class";
+import { ClassData } from "../types/classes";
+import { getConnection } from "typeorm";
 
 export async function show(
-  request: Request,
-  response: Response
-): Promise<void> {}
+  request: Request<{ id: string }, {}, {}, {}>,
+  response: Response<ClassData>
+): Promise<void> {
+  try {
+    const { id } = request.params;
+    const idNum = parseInt(id);
+    if (!idNum) {
+      response.status(400);
+      return;
+    }
+
+    const clazz = await getConnection()
+      .transaction<Class | undefined>(async (manager) => {
+        return await manager.getRepository(Class).findOne({
+          where: { id: idNum },
+          relations: ["programme", "classPersons", "classPersons.person"],
+        });
+      })
+      .then(async (clazz) => {
+        if (!clazz) {
+          response.sendStatus(404);
+          return;
+        }
+
+        const data = await clazz.getData();
+        response.status(200).json(data);
+        return;
+      })
+      .catch((error) => {
+        console.log(error);
+        response.sendStatus(400);
+        return;
+      });
+
+    return;
+  } catch (error) {
+    console.log(error);
+    response.status(400);
+    return;
+  }
+}
 
 export async function edit(
   request: Request,
