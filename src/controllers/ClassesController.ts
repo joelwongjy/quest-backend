6df27a1731d389ba742c820943cbcb15eeba0214
lowjiefeng1998 <lowjiefeng1998@gmail.com>
2021-03-14 20:16:38 +0800

    feat: init the class routes

---
+++ src/controllers/ClassesController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export async function show(
+  request: Request,
+  response: Response
+): Promise<void> {}
+
+export async function edit(
+  request: Request,
+  response: Response
+): Promise<void> {}
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -6,6 +6,7 @@ import seed from "./seed";
 import questionnaire from "./questionnaire";
 import programmes from "./programmes";
 import students from "./students";
+import classes from "./classes";
 
 const routes = Router();
 
@@ -16,5 +17,6 @@ routes.use("/students", students);
 routes.use("/seed", seed);
 routes.use("/questionnaires", questionnaire);
 routes.use("/programmes", programmes);
+routes.use("/classes", classes);
 
 export default routes;
