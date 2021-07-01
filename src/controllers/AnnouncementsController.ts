13cb25dec30b516798408aad0d21aed99f9e1fa0
vineethbuddha <buddhavineeth@gmail.com>
2021-07-01 18:44:04 +0800

    feat: implement filter ability in announcement index controller

--- src/controllers/AnnouncementsController.ts
+++ src/controllers/AnnouncementsController.ts
@@ -5,6 +5,17 @@ import { Class } from "../entities/programme/Class";
 import { Announcement } from "../entities/programme/Announcement";
 import { Programme } from "../entities/programme/Programme";
 import { AnnouncementListData, AnnouncementData } from "../types/announcements";
+import { AccessTokenSignedPayload } from "src/types/tokens";
+import { User } from "../entities/user/User";
+import { DefaultUserRole } from "../types/users";
+
+function convertDateStringToDateObject(date: string | Date) {
+  if (!(date instanceof Date)) {
+    return new Date(date);
+  }
+
+  return date;
+}
 
 export async function create(
   request: Request,
@@ -14,13 +25,9 @@ export async function create(
     let { title, startDate, endDate, programmeIds, classIds, body } =
       request.body;
 
-    if (!(startDate instanceof Date)) {
-      startDate = new Date(startDate);
-    }
-
-    if (!(endDate instanceof Date)) {
-      endDate = new Date(endDate);
-    }
+    // convert date strings to Date objects
+    startDate = convertDateStringToDateObject(startDate);
+    endDate = convertDateStringToDateObject(endDate);
 
     // check for sufficient data to process request
     const isCompleteData = title && startDate && endDate;
@@ -32,16 +39,18 @@ export async function create(
       );
     }
 
+    // load data of associated programmes and classes
     const { programmes, classes } = await addProgrammesAndClassesData(
       programmeIds,
       classIds
     );
 
-    // Assert there is at least 1 programme and 1 class to tag announcement to
+    // assert there is at least 1 programme and 1 class to tag announcement to
     if (programmes.length == 0 || classes.length == 0) {
       throw new Error("Kindly provide valid programmeIds or classIds");
     }
 
+    // create and save new announcement
     const announcement: Announcement = new Announcement(
       startDate,
       endDate,
@@ -81,8 +90,8 @@ async function addProgrammesAndClassesData(
     });
 
     for (const program of programmesTemp) {
-      programmeIdsAdded.push(program.id);
       programmes.push(program);
+      programmeIdsAdded.push(program.id);
 
       for (const class_ of program.classes) {
         classes.push(class_);
@@ -122,6 +131,37 @@ export async function index(
   response: Response
 ): Promise<void> {
   try {
+    const payload = response.locals.payload as AccessTokenSignedPayload;
+    const { userId } = payload;
+    const user = await getRepository(User).findOne({
+      where: { id: userId },
+      select: ["id", "defaultRole"],
+      relations: [
+        "person",
+        "person.classPersons",
+        "person.classPersons.class",
+        "person.classPersons.class.programme",
+      ],
+    });
+
+    console.log(user?.person.classPersons);
+
+    const isAdmin: boolean = user?.defaultRole == DefaultUserRole.ADMIN;
+    const userClassIds: number[] = [];
+    const userProgrammeIds: number[] = [];
+
+    if (!isAdmin && user?.person.classPersons) {
+      for (const classPerson of user?.person.classPersons) {
+        userClassIds.push(classPerson.class.id);
+        if (!userProgrammeIds.includes(classPerson.class.programme.id)) {
+          userProgrammeIds.push(classPerson.class.programme.id);
+        }
+      }
+    }
+
+    console.log("userClassIds", userClassIds);
+    console.log("userProgrammeIds", userProgrammeIds);
+
     let allAnnouncements: Announcement[] = await getRepository(
       Announcement
     ).find({
@@ -136,20 +176,27 @@ export async function index(
 
       if (announcement.programmes) {
         announcement.programmes.map((programme) => {
-          programmeData.push({
-            id: programme.id,
-            name: programme.name,
-          });
+          if (isAdmin || userProgrammeIds.includes(programme.id)) {
+            programmeData.push({
+              id: programme.id,
+              name: programme.name,
+            });
+          }
         });
       }
 
       if (announcement.classes) {
         announcement.classes?.map((class_) => {
-          classData.push({
-            id: class_.id,
-            name: class_.name,
-          });
+          if (isAdmin || userClassIds.includes(class_.id))
+            classData.push({
+              id: class_.id,
+              name: class_.name,
+            });
         });
+      }
+
+      if (programmeData.length == 0 || classData.length == 0) {
+        return 1; // user cannot access this announcement so do not include in result
       }
 
       const currListData: AnnouncementListData = {
